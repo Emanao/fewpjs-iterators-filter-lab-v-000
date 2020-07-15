527ba3f1bb1d65ea0ad98ab37363d5398b3ea39d
Eva Navarro <emanao@me.com>
2020-07-15 23:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function findMatching(drivers, str){
-  console.log(drivers +":"+ str)
   return drivers.filter(driver=>driver.toLowerCase()===str.toLowerCase());
 }
 function fuzzyMatch(drivers, str){
-  console.log(drivers +":"+ str)
   return drivers.filter(driver=>driver.startsWith(str));
 }
