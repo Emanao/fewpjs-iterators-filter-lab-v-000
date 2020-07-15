7294786987ac7028d220293cdb0700d9018e4a09
Eva Navarro <emanao@me.com>
2020-07-15 23:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function findMatching(drivers, str){
   console.log(drivers +":"+ str)
   return drivers.filter(driver=>driver.toLowerCase()===str.toLowerCase());
 }
+function fuzzyMatch(drivers, str){
+  console.log(drivers +":"+ str)
+  return drivers.filter(driver=>driver.toLowerCase()===str.toLowerCase());
+}
