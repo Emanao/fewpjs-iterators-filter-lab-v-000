70044bf13e93513ae77bf25f42c4e5a3dbde436e
Eva Navarro <emanao@me.com>
2020-07-15 23:54:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function findMatching(drivers, str){
   return drivers.filter(driver=>driver.toLowerCase()===str.toLowerCase());
 }
+
 function fuzzyMatch(drivers, str){
   return drivers.filter(driver=>driver.startsWith(str));
+}
+
+function matchName(drivers, str){
+  console.log(driver, str);
+
 }
